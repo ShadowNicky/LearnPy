52e3eb5e6bc21f370d0f58a4f5307d3104875bd0
NIcky <al.al.00@bk.ru>
2020-08-05 21:36:37 +0400

    Second commit

--- Training project/task-1/Ex9.py
+++ Training project/task-1/Ex9.py
@@ -1,6 +1,6 @@
 a = str(input('Введите строку: '))
 b = a.split(' ')
 print('Количество букв \"е\": ' + str(a.count('е')))
-print('Измененная строка: ' + b[0].replace('е', 'и') + a[len(b[0]):len(a)])
+print('Измененная строка : ' + b[0].replace('е', 'и') + a[len(b[0]):len(a)])
 input ()
 
